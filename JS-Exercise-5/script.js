48b2bd4fba274640a629a439d695b074e82f20e4
Jash Vadgama <jashvadgama.ownai@gmail.com>
2026-03-11 21:48:52 +0530

    [Done] JS Exercise 05

--- JS-Exercise-5/script.js
+++ JS-Exercise-5/script.js
@@ -7,63 +7,63 @@
 
 using switch case. */
 
-const stdName = document.getElementById('studentName')
-const mathMarks = document.getElementById('mathMarks')
-const sciMarks = document.getElementById('scienceMarks')
-const engMarks = document.getElementById('engMarks')
-const submitData = document.getElementById('submitData')
+const stdName = document.getElementById("studentName");
+const mathMarks = document.getElementById("mathMarks");
+const sciMarks = document.getElementById("scienceMarks");
+const engMarks = document.getElementById("engMarks");
+const submitData = document.getElementById("submitData");
 
-const stdData = {}
+const stdData = {};
 
 const addData = (name, mathMark, sciMark, engMark) => {
-    const obj = {
-        name: name,
-        mathsMarks: Number(mathMark),
-        scienceMarks: Number(sciMark),
-        englishMarks: Number(engMark)
-    }
-    Object.assign(stdData, obj)
-}
+  const obj = {
+    name: name,
+    mathsMarks: Number(mathMark),
+    scienceMarks: Number(sciMark),
+    englishMarks: Number(engMark),
+  };
+  Object.assign(stdData, obj);
+};
 
-submitData.addEventListener('click', (e) => {
-    e.preventDefault()
+submitData.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    addData(stdName.value, mathMarks.value, sciMarks.value, engMarks.value)
+  addData(stdName.value, mathMarks.value, sciMarks.value, engMarks.value);
 
-    delete stdData.name
+  delete stdData.name;
 
-    let sum = 0
-    for (let i in stdData) {
-        sum += stdData[i]
-    }
+  let sum = 0;
+  for (let i in stdData) {
+    sum += stdData[i];
+  }
 
-    console.log(`Total of all subject marks: ${sum}`);
+  console.log(`Total of all subject marks: ${sum}`);
 
-    switch (true) {
-        case (sum >= 200):
-            console.log("you are passed with grade A");
-            break;
-        case (sum >= 100):
-            console.log("you are passed with grade B");
-            break;
-        case (sum < 100 && sum >= 55):
-            console.log("you are passed with grade C");
-            break;
-        default:
-            console.log("You are failed");
-    }
+  switch (true) {
+    case sum >= 200:
+      console.log("you are passed with grade A");
+      break;
+    case sum >= 100:
+      console.log("you are passed with grade B");
+      break;
+    case sum < 100 && sum >= 55:
+      console.log("you are passed with grade C");
+      break;
+    default:
+      console.log("You are failed");
+  }
+});
 
-})
-
-stdName && mathMarks && sciMarks && engMarks.addEventListener('keyup', () => {
+stdName &&
+  mathMarks &&
+  sciMarks &&
+  engMarks.addEventListener("keyup", () => {
     if (stdName && mathMarks && sciMarks && engMarks.value.trim() === "") {
-        submitData.disabled = true;
+      submitData.disabled = true;
     } else {
-        submitData.disabled = false;
+      submitData.disabled = false;
     }
-})
-
-
+  });
 
 /* 2. Generate an array of random digits like [1,2,3,6,5,3,46,63];
 
@@ -73,47 +73,39 @@ stdName && mathMarks && sciMarks && engMarks.addEventListener('keyup', () => {
  b. Take one button and click on it do like,
     if array contains digit '5' then don't print digit '5' , print all other elements. Output will be [1,2,3,6,3,46,63] */
 
-const generateBtn = document.getElementById('generateBtn')
-const trimArr = document.getElementById('trimArr')
-const skipArr = document.getElementById('skipArr')
+const generateBtn = document.getElementById("generateBtn");
+const trimArr = document.getElementById("trimArr");
+const skipArr = document.getElementById("skipArr");
 let arr = [];
 
-generateBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const arrayLength = document.getElementById('arrayLength').value
-    for (var i = 0; i < arrayLength; i++) {
-        arr.push(Math.round(Math.random() * 30))
+  arr = []
+
+  const arrayLength = document.getElementById("arrayLength").value;
+  for (var i = 0; i < arrayLength; i++) {
+    arr.push(Math.round(Math.random() * 30));
+  }
+//   console.log(arr);
+  console.table(arr)
+});
+
+trimArr.addEventListener("click", (e) => {
+  e.preventDefault();
+  let result = [];
+  for (let num of arr) {
+    if (num === 5) {
+      break;
     }
-    console.log(arr);
-})
+    result.push(num);
+  }
+  console.table(result);
+});
 
-trimArr.addEventListener('click', (e) => {
-    e.preventDefault()
-    let result = []
-    for (let num in arr) {
-        if (num === 5) break;
-    }
-    result.push(arr)
-    console.log(result);
-})
+skipArr.addEventListener("click", (e) => {
+  e.preventDefault();
 
-skipArr.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    const finalArr = arr.filter(n => n !== 5)
-    console.log(finalArr);
-})
-
-
-// if(arr.includes(5)){
-//     for(let i = 0; i<= arr.indexOf(5); i++){
-//         console.log("Digits till 5:", arr[i]);
-//     }
-//     console.log("Index of Digit 5:", arr.indexOf(5));
-// }
-
-
-
-
-
+  const finalArr = arr.filter((n) => n !== 5);
+  console.table(finalArr);
+});
